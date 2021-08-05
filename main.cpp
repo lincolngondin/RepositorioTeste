693eeb089ca4ee6b90381afb99c67f29846ad0e8
Lincoln Monteiro <lincolngondim@gmail.com>
2021-08-05 16:42:34 -0300

    Adicao da funcao produto

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
+#include <cstdarg>
+
+
+
+int produto(int n, ...){
+	va_list arg;
+	va_start(arg, n);
+	int t = 1;
+	for(int i = 0; i< n; i++)
+		t*= va_arg(arg, int);
+	
+	va_end(arg);
+	return t;
+}
+
 int main(){
-	std::cout << "Ola mundo\n"; 
+	std::cout << produto(4, 2, 3, 4, 6); 
 	
 	return 0;
 }
